6cd01ff4f2f5bbf9f1289872d2a499b07970791d
Leah Young <leah.young21@alumni.colostate.edu>
2022-11-15 12:16:48 -0700

    remove formatting on app page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,39 @@
-import React, { Component } from "react";
-import "./App.css";
-import { getUrls, postUrls } from "../../apiCalls";
-import UrlContainer from "../UrlContainer/UrlContainer";
-import UrlForm from "../UrlForm/UrlForm";
+import React, { Component } from 'react';
+import './App.css';
+import { getUrls, postUrls } from '../../apiCalls';
+import UrlContainer from '../UrlContainer/UrlContainer';
+import UrlForm from '../UrlForm/UrlForm';
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       urls: [],
-      error: "",
-    };
+      error: ''
+    }
   }
 
   componentDidMount() {
-    getUrls().then((data) => this.setState({ urls: data.urls }));
-    // .catch(error => this.setState({error: `Uh-oh! There's a "${error.message}" error! Try again! `}))
+    getUrls()
+      .then(data => this.setState({urls: data.urls}))
+      // .catch(error => this.setState({error: `Uh-oh! There's a "${error.message}" error! Try again! `}))
   }
 
   addURLS = (newURL) => {
-    postUrls(newURL).then((data) =>
-      this.setState({ urls: [...this.state.urls, data] })
-    );
-  };
+    postUrls(newURL)
+      .then(data => this.setState({urls: [...this.state.urls, data]}))
+  }
 
   render() {
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm addUrls={this.addURLS} />
+          <UrlForm addUrls={this.addURLS}/>
         </header>
         {/* {this.state.error ? <h2 className='error-msg'>{this.state.error}</h2> : null} */}
 
-        <UrlContainer urls={this.state.urls} />
+        <UrlContainer urls={this.state.urls}/>
       </main>
     );
   }
