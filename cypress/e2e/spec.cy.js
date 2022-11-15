cd246a199459236a827e76c4bf9965dcb6fa11ca
Leah Young <leah.young21@alumni.colostate.edu>
2022-11-15 11:14:37 -0700

    add fixtures for get and post, start Cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,17 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Page load', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 200,
+      ok: true,
+      fixture: 'sampleData.json'
+    })
+    cy.visit('http://localhost:3000')
   })
+  it('should display title and existing shortened urls', () => {
+    cy.get('h1').should('exist').should('contain', 'URL Shortener')
+    
+  })
+  // cy.get('[placeholder="Title..."]').should('exist')
+  // cy.get('[placeholder="URL to Shorten..."]').should('exist')
+  // cy.get('button').should('exist')
 })
